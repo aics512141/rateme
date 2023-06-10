ec8ba063cf3749bbebcc091788759fe66d3c2b96
aics512141 <114088632+aics512141@users.noreply.github.com>
2023-06-10 05:52:00 -0700

    client day 18

--- client/src/components/employees/Employees.js
+++ client/src/components/employees/Employees.js
@@ -1,6 +1,6 @@
 import { Avatar, Box, Button, IconButton, Table, TableBody, TableCell, TableHead, TableRow, Typography } from "@mui/material";
 import { connect } from "react-redux";
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import { loadDepartments } from "../../store/actions/departmentActions";
 import { useEffect } from "react";
 import AddIcon from '@mui/icons-material/Add';
@@ -8,6 +8,8 @@ import RefreshIcon from '@mui/icons-material/Refresh';
 import EditIcon from '@mui/icons-material/Edit';
 
  function Employees() {
+  const { deptId } = useParams()
+    
 
     useEffect(() => {
         
@@ -20,8 +22,8 @@ import EditIcon from '@mui/icons-material/Edit';
                 <Box display="flex" justifyContent="space-between">
                     <Typography variant="h5"> Employees</Typography>
                     <Box>
-                        <Button component={Link} to="/admin/employees/add" variant="outlined" startIcon={<AddIcon />}>Add</Button>
-                        <Button sx={{ ml: 1 }} onClick={loadDepartments} variant="outlined" endIcon={<RefreshIcon />}>Refresh</Button>
+                        <Button component={Link} to="/admin/employees/add" variant="outlined" startIcon={<AddIcon />}>Add Employess</Button>
+                        <Button sx={{ ml: 1 }} component={Link} to={`/admin/departments/edit/${deptId}`} variant="outlined" startIcon={<EditIcon/>}>Edit Department Info</Button>
                     </Box>
                 </Box>
             </Box>
