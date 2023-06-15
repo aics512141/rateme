28e65a6713e68e6fae2b7812f78d9a8170db2b85
aics512141 <114088632+aics512141@users.noreply.github.com>
2023-06-15 02:01:53 -0700

    client day 20

--- client/src/components/employees/Employees.js
+++ client/src/components/employees/Employees.js
@@ -1,4 +1,4 @@
-import { Avatar, Box, Button, IconButton, Table, TableBody, TableCell, TableHead, TableRow, Typography } from "@mui/material";
+import { Avatar, Box, Button, IconButton, Pagination, Table, TableBody, TableCell, TableHead, TableRow, TextField, Typography } from "@mui/material";
 import { connect, useDispatch } from "react-redux";
 import { Link, useParams } from "react-router-dom";
 import { loadEmployees } from "../../store/actions/departmentActions";
@@ -8,8 +8,9 @@ import RefreshIcon from '@mui/icons-material/Refresh';
 import EditIcon from '@mui/icons-material/Edit';
 import { hideProgressBar, showProgressBar } from "../../store/actions/progressBarAction";
 import axios from "axios";
-import { showError } from "../../store/actions/alertActions";
+import { showError, showSuccess } from "../../store/actions/alertActions";
 import DeleteEmployee from "./DeleteEmployee";
+import EmployeeQRCode from "./EmployeeQRCode";
 
  function Employees() {
 
@@ -17,12 +18,16 @@ import DeleteEmployee from "./DeleteEmployee";
     const dispatch = useDispatch();
     const [department, setDepartment] = useState(null);
     const [employees, setEmployees] = useState([]);
+    const [page, setPage] = useState(1);
+    const [numOfPages, setNumOfPages] = useState(1);
+    const [query, setQuery] = useState("")
 
     const loadEmployees = () => {
         dispatch(showProgressBar());
-        axios.post('/api/employees/search',{deptId}).then(result => {
+        axios.post('/api/employees/search',{deptId, page, query}).then(result => {
             setDepartment(result.data.department);
             setEmployees(result.data.employees);
+            setNumOfPages(result.data.numOfPages)
             dispatch(hideProgressBar())
         }).catch(error => {
             let message = error && error.response && error.response.data ? error.response.data.error : error.message;
@@ -30,10 +35,26 @@ import DeleteEmployee from "./DeleteEmployee";
             dispatch(showError(message))
         })
       }
+
+      const deleteEmployee = (id) => {
+        dispatch(showProgressBar())
+        axios.post('api/employees/delete', { id }).then(({ data }) => {
+            if (data.success) {
+                dispatch(showSuccess('Department deleted successfully'))
+                dispatch(hideProgressBar())
+                setEmployees( employees => employees.filter(item => item._id !== id) );
+
+            }
+          }).catch(error => {
+            dispatch(hideProgressBar())
+            let message = error && error.response && error.response.data ? error.response.data.error : error.message;
+            dispatch(showError(message))
+          })
+         }
     
         useEffect(() => {
             loadEmployees()
-        },[])
+        },[page])
 
     if(!department) return null;
     return(
@@ -43,10 +64,14 @@ import DeleteEmployee from "./DeleteEmployee";
                 <Box display="flex" justifyContent="space-between">
                     <Typography variant="h5"> Employees</Typography>
                     <Box>
-                        <Button component={Link} to="/admin/departments/add" variant="outlined" startIcon={<AddIcon />}>Add</Button>
+                        <Button component={Link} to={`/admin/employees/add/${deptId}`} variant="outlined" startIcon={<AddIcon />}>Add Employee</Button>
                         <Button sx={{ ml: 1 }} onClick={loadEmployees} variant="outlined" endIcon={<RefreshIcon />}>Refresh</Button>
                     </Box>
                 </Box>
+            </Box>
+            <Box display="flex" justifyContent="space-between" alignItems="center" mt={2}>
+                 <TextField sx={{flexGrow: 1, mr:2}} onChange={(event) => setQuery(event.target.value)} placeholder="Search Name, Email,cnic,Phone,desigination etc" size="small" />
+                 <Button variant="contained" onClick={loadEmployees}>Search</Button>
             </Box>
             <Table>
                 <TableHead>
@@ -80,8 +105,8 @@ import DeleteEmployee from "./DeleteEmployee";
                                 <TableCell> { employee.cnic }</TableCell>
                                 <TableCell> 
                                 <IconButton component={Link} to={`/admin/employees/edit/${employee._id}`}><EditIcon /></IconButton> 
-                                <DeleteEmployee employeeId={employee._id} name={employee.name} />
-
+                                <DeleteEmployee employeeId={employee._id} name={employee.name} deleteEmployee={deleteEmployee} />
+                                <EmployeeQRCode name = {employee.name} employeeId={employee._id} />
                                 </TableCell>
                             </TableRow>
                         ))
@@ -89,6 +114,9 @@ import DeleteEmployee from "./DeleteEmployee";
                 </TableBody>
 
             </Table>
+            <Box display="flex" justifyContent="center" mt={3}>
+            <Pagination count={numOfPages} variant="outlined" color="primary" page={page} onChange={(event,value) => setPage(value)} />
+           </Box>
         </Box>
     )
   }
